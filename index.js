295a7b05accc0e155c0e0a3080d4cfdc5ea65c8d
Israel Perez <isra.perez.2000@gmail.com>
2026-05-12 11:56:14 +0200

    feat: url storage and recovery

--- index.js
+++ index.js
@@ -17,11 +17,20 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+var url = [];
+var original_url;
+var short_url;
+
+
 // 1. POST a URL to /api/shorturl, and get a JSON response with original_url and short_url properties
 app.post('/api/shorturl', function (req,res){
   // recover url is in req.body.url (input type='text' name='url')
-  var original_url = req.body.url;
-  var short_url = 1;
+  url.push(req.body.url)
+  // array.indexOf("arrayValue") --> will return value position
+  console.log(url+" : "+url.length);
+
+  original_url = url.find((value) => value == req.body.url);
+  short_url = url.indexOf(original_url);
 
   res.json({"original_url": original_url, "short_url":short_url})
 })
