bc1126058553c86f879b456ac386386b8fd77e0f
Israel Perez <isra.perez.2000@gmail.com>
2026-05-12 11:09:26 +0200

    Initial project commit

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// 1. POST a URL to /api/shorturl, and get a JSON response with original_url and short_url properties
+
+// 2. When visiting /api/shorturl/<short_url> --> redirect to original URL
+
+// 3. If entered URL is invalid return JSON error response
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
