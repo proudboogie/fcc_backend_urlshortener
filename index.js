bb4c9afe93b478d9a4d34f0666830bc19bdc9f5a
Israel Perez <isra.perez.2000@gmail.com>
2026-05-13 17:00:01 +0200

    fix: 3rd test approved

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const bodyParser = require('body-parser');
 // Basic Configuration
 const port = process.env.PORT || 3000;
+// Initialize bodyParser
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(cors());
 
@@ -15,43 +17,50 @@ app.get('/', function(req, res) {
 });
 
 
-// Initialize bodyParser
-app.use(bodyParser.urlencoded({extended:false}));
 
 var url = [];
 
 // 1. POST a URL to /api/shorturl, and get a JSON response with original_url and short_url properties
 app.post('/api/shorturl', function (req,res){
   // recover url is in req.body.url (input type='text' name='url')
+  
+  // check http:// OR https:// use
+  if(req.body.url.startsWith("https://") || req.body.url.startsWith("http://")){
 
-  // If url is not stored yet add new url
-  if(!url.includes(req.body.url)){
-    url.push(req.body.url)
+    // If url is not stored yet add new url
+    if(!url.includes(req.body.url)){
+      url.push(req.body.url)
+    }
+    // array.find(function) will return the first matching value
+    let originalUrl = url.find((value) => value == req.body.url);
+    
+    // array.indexOf("arrayValue") --> will return value position
+    let shortUrl = url.indexOf(originalUrl);
+
+    res.json({"original_url": originalUrl, "short_url":shortUrl})
+
+    //console.log('POST URL','\n', 'req.body: ',req.body, '\n', 'req.params: ', req.params, '\n', {"original_url": original_url, "short_url":short_url})
   }
-  // array.find(function) will return the first matching value
-  let original_url = url.find((value) => value == req.body.url);
+  // 3. If entered URL is invalid return JSON error response
+  else{
+    res.json({error:'Invalid url'})
+  }
 
-  // array.indexOf("arrayValue") --> will return value position
-  let short_url = url.indexOf(original_url);
-
-  res.json({"original_url": original_url, "short_url":short_url})
 })
 
 // 2. When visiting /api/shorturl/<short_url> --> redirect to original URL
 app.get('/api/shorturl/:short_url', function(req,res){
   let original_url = url[req.params.short_url];
 
-  console.log(original_url)
   if(original_url){
     res.redirect(original_url);
   }
   else{
     res.json({error:'there is no short URL for that input'})
   }
+  
+  //console.log('GET URL','\n', 'req.body: ',req.body, '\n', 'req.params: ', req.params, '\n', 'redirecting to :',original_url ,' with: ',req.params.short_url)
 })
-// 3. If entered URL is invalid return JSON error response
-
-
 
 
 app.listen(port, function() {
